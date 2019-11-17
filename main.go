4d628d9ade34220b5375580e8db7475ceadc0b35
Ralph Olutola <oreofe.olutola@gmail.com>
2019-11-17 09:54:36 +0100

    first golang program

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world, this is my first golang project")
+	name := "Oreofeoluwapo Olutola"
+	fmt.Printf("My name is %s\n", name)
+}
