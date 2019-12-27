b364c17f1bdb35215d2e7fb3fff003d0768b2072
Ralph Olutola <oreofe.olutola@gmail.com>
2019-12-27 09:33:34 +0100

    Write into file

--- 07-read-line-by-line/main.go
+++ 07-read-line-by-line/main.go
@@ -9,17 +9,32 @@ import (
 
 func main() {
 	file, err := os.Open("./data/07-line-by-line.txt")
-	defer file.Close()
+	fileToWrite, fileToWriteErr := os.Create("./data/07-line-by-line-write.txt")
 
-	if err != nil {
+	defer file.Close()
+	defer fileToWrite.Close()
+
+	if err != nil || fileToWriteErr != nil {
 		panic(err)
 	}
 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
+	writer := bufio.NewWriter(fileToWrite)
+
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		text := scanner.Text()
+		// fmt.Println(text)
+		byteWritten, writeErr := writer.WriteString(text + "\n")
+
+		if writeErr != nil {
+			fmt.Printf("Error writing %s into the file %s \n", text, fileToWrite.Name())
+			panic(writeErr)
+		}
+
+		fmt.Printf("wrote %s (%d bytes) into file %s \n", text, byteWritten, fileToWrite.Name())
+		writer.Flush()
 		time.Sleep(1 * time.Second)
 	}
 
